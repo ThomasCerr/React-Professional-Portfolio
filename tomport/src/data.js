145e25d9d1d54fce142279fe2ec04072b0f6614a
ThomasCerr <99366289+ThomasCerr@users.noreply.github.com>
2022-07-20 19:03:18 -0400

    About and Projects components complete, data.js as well

---
+++ tomport/src/data.js
@@ -0,0 +1,35 @@
+
+export const projects = [
+    {
+      title: "Project 1",
+      subtitle: "Subtitle here",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-1.gif",
+      link: "",
+    },
+    {
+      title: "Music Suggestion App",
+      subtitle: "Node, MySQL, Javascript, Express",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-2.gif",
+      link: "https://tranquil-retreat-40972.herokuapp.com/search",
+    },
+    {
+      title: "TABS Brewery & Weather App",
+      subtitle: "Javascript, CSS, HTML, API",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-3.gif",
+      link: "https://thomascerr.github.io/TABS-Brewery-Weather-Application/",
+    },
+    {
+      title: "Project 4",
+      subtitle: "Subtitle here",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.gif",
+      link: "",
+    },
+  ];
